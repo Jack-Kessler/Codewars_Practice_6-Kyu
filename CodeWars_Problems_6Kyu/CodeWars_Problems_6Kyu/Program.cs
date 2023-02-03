43e8e7eb00b1e1f6c364784657233db9350ea31b
Jack Kessler <jack_kessler@hotmail.com>
2023-02-03 11:46:40 -0600

    Recursion Notes

--- CodeWars_Problems_6Kyu/CodeWars_Problems_6Kyu/Program.cs
+++ CodeWars_Problems_6Kyu/CodeWars_Problems_6Kyu/Program.cs
@@ -1,4 +1,111 @@
-﻿//-----------------------------------------------------------------------------------------------------------------------------
+﻿// Learning Recurssion
+
+//int Factorial(int number)
+//{
+//    var factorialProduct = 1;
+
+//    return number < 0 ? throw new ArgumentOutOfRangeException() //short hand way
+//        : number == 0 ? factorialProduct
+//        : number * Factorial(number -1);
+
+//    //if (number < 0) // More basic way
+//    //{
+//    //    throw new ArgumentOutOfRangeException();
+//    //}
+
+//    //return number == 0 ? factorialProduct : number * Factorial (number - 1);
+//}
+
+//int x = Factorial(4);
+//Console.WriteLine(x);
+
+
+
+//Simple Fun #52: Pair Of Shoes
+
+//int[][] shoes =
+//    {
+//    new int[] { 0, 21 },
+//    new int[] { 1, 23 },
+//    new int[] { 1, 21 },
+//    new int[] { 0, 23 }
+//    };
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+//Simple Reversed Parenthesis (6-Kyu)
+
+//string s = "())(((";
+//int counter = 0;
+//int i = 0;
+//int isEven = s.Length % 2;
+
+//if (isEven != 0)
+//{
+//    Console.WriteLine(-1);
+//    //return -1;
+//}
+
+//for (i = 0; i < s.Length - 1; i++) // Will truncate string to either only '(' or ')' with the exception of possibly first or last element
+//{
+//    if (s[0] == ')')
+//    {
+//        s = '(' + s.Substring(1);
+//        counter++;
+//        i = -1;
+//    }
+//    else if (s[s.Length - 1] == '(')
+//    {
+//        s = s.Remove(s.Length - 1);
+//        s = s + ")";
+//        counter++;
+//        i = -1;
+//    }
+//    else if (s[i] == '(' && s[i + 1] == ')')
+//    {
+//        s = s.Remove(i, 2);
+//        i = -1;
+//    }
+//}
+
+//Console.WriteLine(counter);
+//return counter;
+
+
+//better solution below
+
+//int solve(string s)
+//{
+//    if (string.IsNullOrEmpty(s)) return 0;                                // Line 1
+//    else if (s.Length % 2 == 1) return -1;                                // Line 2
+//    else if (s.Contains("()")) return solve(s.Replace("()", ""));         // Line 3
+//    return (s.Substring(0, 2) == ")(" ? 2 : 1) + solve(s.Substring(2));   // Line 4
+    
+//         *** Below is the general flow of what is happening above: ***
+//      A string is read in.
+//      If the string is empty or null, line 1 code will be implemented and return o. Done.
+
+//      If the string length is odd, this challenge of having all parentheses be balanced is impossible.
+//      Line 2 code will be implemented and return -1. Done.
+
+//      If the string is non-empty and even length, then code on Line 3 will be implemented. 
+//          Line 3 code removes all "()" from original string.
+
+//      After Line 3, C# will move on to Line 4.
+//          Line 4 will first check to see if the 1st and 2nd character in the string are ")(". If so, return 2 + (following part)...
+//              ... If not, return 1 + (following part).
+
+//          following part: The first go through of line 4 only looked at 1st and 2nd position in string
+//                              ... but there are potentially more than just two characters in the string at this point.
+//                              ... therefore, the second half of Line 4 code redoes the whole process explained above...
+//                              ... starting from position 3 (i.e. s[2]).
+//                              ... Eventually there will only be 2 characters and when the 2nd half runs one more time...
+//                              ... Line 1 will activate (because it will be null / empty) and return 0.
+//                              ... At this point, solve will add up all the 1s and 2s from Line 4 to return one final int.
+
+//}
+
+//-----------------------------------------------------------------------------------------------------------------------------
 //Sudoku board validator (6-Kyu)
 //using System.Collections.Concurrent;
 //using System.Reflection.Metadata.Ecma335;
